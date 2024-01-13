2c84f1196a961e28c9c413b964e3c22917573467
Nauman Shabbir <mnaumanshabbir@gmail.com>
2024-01-13 15:55:47 +0500

    vercel testing

--- Backend/index.js
+++ Backend/index.js
@@ -13,21 +13,51 @@ app.use(cors())
 app.use(express.json())
 
 // Routes
+function getData(data) {
+    console.log("data", data);
+    return data;
+}
+app.get("/api", async (req, res) => {
+    try {
+        const fs = require("fs");
+        var CloudmersiveConvertApiClient = require('cloudmersive-convert-api-client');
 
+        var defaultClient = CloudmersiveConvertApiClient.ApiClient.instance;
+        var Apikey = defaultClient.authentications['Apikey'];
+        Apikey.apiKey = "2f6a6134-8321-4531-bc6f-208ee71bd3e8"
 
-// conection with mongoose
-// mongoose.connect(process.env.CONNECTION_STRING ,{
+        var api = new CloudmersiveConvertApiClient.ConvertDataApi()
 
-// })
+        var inputFile = fs.readFileSync("./files/Hello.docx");
 
-app.get("/", (req, res) => {
-    res.send("Hello Word nauman")
-})
+        // Process the file and send the response
+        // var callback = function (error, data, response) {
+        //     console.log("Hello")
+        //     console.log("data", data)
+        //     console.log("response", response)
+        //     return;
+        //     if (error) {
+        //         console.error(error);
+        //         res.status(500).send("Internal Server Error");
+        //     } else {
+        //         console.log('API called successfully. Returned data: ' + data);
+        //         res.json("Hello Nomi The Sexy Bitch!");
+        //         console.log(response);
+        //     }
+        // };
+        // console.log(callback);
+        // console.log("Before API call");
+        var data = api.convertDataCsvToJson(Buffer.from(inputFile.buffer));
+        getData(data);
+        // console.log("data", data)
+        // console.log("After API call");
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Internal Server Error: " + error.message);
+    }
+});
 
-
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
-})
-
-
+    console.log(`Server is running on port ${PORT}`);
+});
